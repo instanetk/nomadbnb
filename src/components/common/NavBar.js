a68e94f9f47bd1bd2a3bca569dae35180dae045e
David Quintero <davidq@gmail.com>
2020-12-01 15:37:33 -0500

    responsive and brand improvements

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -4,9 +4,12 @@ const NavBar = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <header className=" bg-gray-900">
+    <header className="bg-gray-900 sm:flex sm:justify-between sm:items-center">
       <div className="flex justify-between leading-tight px-4 py-3">
-        <div>
+        <div className="select-none">
+          <span aria-label="beach with umbrella emoji" className="text-2xl">
+            🏖{' '}
+          </span>
           <span className="text-white text-lg">nomad</span>
           <span className="text-green-500 text-lg font-semibold">bnb</span>
         </div>
@@ -39,33 +42,33 @@ const NavBar = () => {
           </button>
         </div>
       </div>
-      <nav className={isOpen ? 'block' : 'hidden'}>
-        <div className="px-2 py-2 border-b border-gray-800">
+      <nav className={`sm:flex sm:items-center ${isOpen ? '' : 'hidden'}`}>
+        <div className="px-2 py-2 border-b border-gray-800 sm:border-b-0 sm:flex sm:px-0">
           <a
             href="#property"
-            className="px-3 py-1 rounded block leading-tight font-semibold text-white hover:bg-gray-800">
+            className="block px-3 py-1 rounded leading-tight font-semibold text-white hover:bg-gray-800 sm:text-sm">
             List your property
           </a>
           <a
             href="#property"
-            className="mt-1 px-3 py-1 rounded block leading-tight font-semibold text-white hover:bg-gray-800">
+            className="mt-1 block sm:mt-0 px-3 py-1 rounded leading-tight font-semibold text-white hover:bg-gray-800 sm:text-sm">
             Trips
           </a>
           <a
             href="#property"
-            className="mt-1 px-3 py-1 rounded block leading-tight font-semibold text-white hover:bg-gray-800">
+            className="mt-1 block sm:mt-0 px-3 py-1 rounded leading-tight font-semibold text-white hover:bg-gray-800 sm:text-sm">
             Messages
           </a>
         </div>
-        <div className="px-5 py-5">
+        <div className="px-5 py-5 sm:py-0 sm:px-4">
           <div className="flex items-center">
             <img
-              className="h-10 w-10 object-cover rounded-full border-2 border-gray-600"
-              src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1651&q=80"
+              className="h-10 w-10 sm:h-9 sm:w-9 object-cover rounded-full border-2 border-gray-600"
+              src="https://images.unsplash.com/photo-1485178575877-1a13bf489dfe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1591&q=80"
               alt="Isla Schoger"></img>
-            <span className="ml-4 text-gray-200 font-semibold">Isla Schoger</span>
+            <span className="ml-4 text-gray-200 font-semibold sm:hidden">Isabela Terragona</span>
           </div>
-          <div className="mt-5">
+          <div className="mt-5 sm:hidden">
             <a href="#account" className="block text-gray-400 hover:text-white">
               Account settings
             </a>
